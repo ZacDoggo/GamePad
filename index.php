ebfa5b576aa75b82e9ef20149b022c9989a8fb73
ZacDoggo <hugo.pecantet@gmail.com>
2021-07-29 14:18:02 +0200

    disable texte selecte

--- index.php
+++ index.php
@@ -10,8 +10,16 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     
     <style>
+      
 
-
+    .disable-select {
+      user-select: none; /* supported by Chrome and Opera */  
+      -webkit-user-select: none; /* Safari */
+      -khtml-user-select: none; /* Konqueror HTML */
+      -moz-user-select: none; /* Firefox */
+      -ms-user-select: none; /* Internet Explorer/Edge */
+      cursor: not-allowed !important;
+    }
 
     .axes {
     padding: 1em;
